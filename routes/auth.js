f5b42f02c1d39e396c872bf3b5a6b8a448ca8287
Ashvin <ashvinsunga@yahoo.com.ph>
2022-05-31 20:31:37 +0800

    added jsonwebtoken

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,14 @@
 
 // import models
 const { User } = require('../models/user');
+
+// import third party modules
 const { Router } = require('express');
 const router = Router();
 const lodash = require('lodash');
 const Joi = require('joi');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.get('/', async (req, res) => {
   const result = await User.find().sort('name');
@@ -26,42 +29,8 @@ router.post('/', async (req, res) => {
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send('Invalid email or password.');
 
-  res.send(true);
-});
-
-// Update information
-router.put('/:id', async (req, res) => {
-  // Validate user input
-  const { error } = validate(req.body);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
-  // Check if the id info does exist
-  const user = await User.findByIdAndUpdate(
-    req.params.id,
-    {
-      name: req.body.name,
-    },
-    { new: true }
-  );
-
-  if (!user) {
-    return res.status(404).send('The user was not found with the given ID');
-  }
-
-  // Update the information
-  return res.send(user);
-});
-
-// Delete an information
-router.delete('/:id', async (req, res) => {
-  // Check if the id info does exist
-  const user = await User.findByIDAndRemove(req.params.id);
-  if (!user) {
-    return res.status(404).send('The user was not found with the given ID');
-  }
-
-  return res.send(user);
+  const token = jwt.sign({ _id: user._id }, 'jwtPrivateKey');
+  res.send(token);
 });
 
 const validate = (req) => {
